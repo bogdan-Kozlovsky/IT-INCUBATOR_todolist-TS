fb4bde7924d53e236d8ed3b30fa9d396417e1fd5
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-03-29 07:58:00 +0300

    16_lesson

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -25,7 +25,7 @@ export const todolistsAPI = {
     getTasks(todolistId: string) {
         return instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`);
     },
-    deleteTask( taskId: string,todolistId: string,) {
+    deleteTask(todolistId: string, taskId: string) {
         return instance.delete<ResponseType>(`todo-lists/${todolistId}/tasks/${taskId}`);
     },
     createTask(todolistId: string, title: string) {
@@ -36,7 +36,25 @@ export const todolistsAPI = {
     }
 }
 
+export const authAPI = {
+    login(data: LoginParamsType) {
+        return instance.post<LoginParamsType, AxiosResponse<ResponseType<{ userId: string }>>>('auth/login', data)
+    },
+    me() {
+        return instance.get<ResponseType<{ id: number, email: string, login: string }>>('/auth/me')
+    },
+    logout() {
+        return instance.delete<ResponseType>('auth/login')
+    }
+}
+
 // types
+export type LoginParamsType = {
+    email: string
+    password: string
+    rememberMe?: Boolean
+    captcha?: string
+}
 export type TodolistType = {
     id: string
     title: string
@@ -49,12 +67,14 @@ export type ResponseType<D = {}> = {
     fieldsErrors: Array<string>
     data: D
 }
+
 export enum TaskStatuses {
     New = 0,
     InProgress = 1,
     Completed = 2,
     Draft = 3
 }
+
 export enum TaskPriorities {
     Low = 0,
     Middle = 1,
@@ -62,6 +82,7 @@ export enum TaskPriorities {
     Urgently = 3,
     Later = 4
 }
+
 export type TaskType = {
     description: string
     title: string
@@ -74,14 +95,19 @@ export type TaskType = {
     order: number
     addedDate: string
 }
-export type UpdateTaskModelType = {
-    title: string
-    description: string
-    status: TaskStatuses
-    priority: TaskPriorities
-    startDate: string
-    deadline: string
-}
+// export type UpdateTaskModelType = {
+//     title: string
+//     description: string
+//     status: TaskStatuses
+//     priority: TaskPriorities
+//     startDate: string
+//     deadline: string
+// }
+
+// export type UpdateTaskModelType = Omit<TaskType, 'id' | 'todoListId' | 'order' | 'addedDate'>
+export type UpdateTaskModelType = Pick<TaskType, 'description' | 'title' | 'status' | 'priority' | 'startDate' | 'deadline'>
+
+
 type GetTasksResponse = {
     error: string | null
     totalCount: number
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { store } from './state/store';
+import App from './app/App';
+import {Provider} from 'react-redux';
+import {store} from './app/store';
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App/>
+            <HashRouter>
+                <App/>
+            </HashRouter>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
