d5562872e029bdf375cf10d28d0036e99235601f
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-07-08 20:36:51 +0300

    Updates

---
+++ precache-manifest.fa41f8f9d9a1111aa8229c4254be3f68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690707db7cc87cd28e0f7b20a4c1a91b",
+    "url": "/IT-INCUBATOR_todolist-TS/index.html"
+  },
+  {
+    "revision": "39392ac9051ace523e03",
+    "url": "/IT-INCUBATOR_todolist-TS/static/css/main.3e1f86ac.chunk.css"
+  },
+  {
+    "revision": "fa7723755833780d5757",
+    "url": "/IT-INCUBATOR_todolist-TS/static/js/2.b925a995.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/IT-INCUBATOR_todolist-TS/static/js/2.b925a995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39392ac9051ace523e03",
+    "url": "/IT-INCUBATOR_todolist-TS/static/js/main.8500c9f7.chunk.js"
+  },
+  {
+    "revision": "6a85607e7934ea55503c",
+    "url": "/IT-INCUBATOR_todolist-TS/static/js/runtime-main.977148bf.js"
+  },
+  {
+    "revision": "7825c3043bf5ba1ee3ff3aa14b11e4fb",
+    "url": "/IT-INCUBATOR_todolist-TS/static/media/logo.7825c304.svg"
+  }
+]);
